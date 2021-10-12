40d8eecabd0c2eb7563483836277f611f2fb16b9
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-10-12 16:56:45 -0300

    created models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9)
+    name = models.CharField(max_length=11)
+    name = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    LEVEL = {
+        ('B', 'BASIC'),
+        ('I', 'INTERMEDIATE'),
+        ('A', 'ADVANCED')
+    }
+
+    course_code = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
+    level = models.CharField(max_length=1, choices=LEVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.description
